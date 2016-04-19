a97081bec95e8776681e9ddb5c2e3190fd5a6c12
pacoma93 <s188283@stud.hioa.no>
2016-04-19 16:39:06 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="eng">
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+		<title>The Library</title>
+		<link rel="stylesheet" type="text/css" href="Styles/Style.css">
+		<script type="text/javascript" src="javascript.js"></script>
+	</head>
+
+	<body>
+		<header>
+			<nav>
+                            <a class="bucket-link" href="Authors.php">Authors</a>
+				<a href="index.php" ><img src="B-ribbon.png" class="welcome-link-img" alt="Turqoise ribbon-link with a curly B on."/> </a> 
+                                <a class="done-link" href="Books.php">Books</a>
+			</nav>
+		</header>
+
+
+	
+		<div id="container"> <!--An invisible container to keep my content in the middle of the page-->
+			<h1 id="welcome">Welcome to The Library</h1>
+                        <p id="about">One of the best libraries in the country!</p>
+
+			<a href="http://www.urbandictionary.com/define.php?term=library" target="blank">what is a library? Click on me and find out!</a>
+			
+	
+			
+		</div>
+
+	</body>
+
+</html>
